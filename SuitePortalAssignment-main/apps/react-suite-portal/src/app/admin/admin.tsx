bb2aaed258fa38972d361530f0b4feeaabd88295
Jenna <jenna.lapstra@gmail.com>
2020-11-13 16:05:05 -0500

    React Navbar Created, New Components to Write Next

---
+++ SuitePortalAssignment-main/apps/react-suite-portal/src/app/admin/admin.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import './admin.css';
+
+/* eslint-disable-next-line */
+export interface AdminProps {}
+
+export const Admin = (props: AdminProps) => {
+  return (
+    <div>
+      <h1>Welcome to Admin!</h1>
+    </div>
+  );
+};
+
+export default Admin;
